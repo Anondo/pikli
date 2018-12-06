dad00d966c3c417e6328eff53cc2f8163e5400c8
Anondo <aanondos@gmail.com>
2018-12-06 15:41:37 +0600

    provided documentation

--- pikli/__init__.py
+++ pikli/__init__.py
@@ -1,1 +1,30 @@
+"""
+
+  Pikli CLI Library
+  ~~~~~~~~~~~~~~~~~
+
+  Copyright @2018 Ahmad Anondo.  All rights reserved.
+  Use of this source code is governed by a MIT-style
+  license that can be found in the LICENSE file.
+
+  Pikli is a library , written in python to creat cli interface based apps.
+
+  usage:
+
+        >>> import pikli
+        >>> root_command = pikli.Command(
+        ...     use = "hello",
+        ...     short = "hello is the first ever cli app built using pikli"
+        ... )
+        >>> root_command.execute()
+        hello is the first ever cli app built using pikli
+
+
+
+"""
+
+
+
+
+
 from . command import Command
--- pikli/command.py
+++ pikli/command.py
@@ -1,6 +1,46 @@
+
+"""
+  pikli.command
+  ~~~~~~~~~~~~~
+
+  Copyright @2018 Ahmad Anondo.  All rights reserved.
+  Use of this source code is governed by a MIT-style
+  license that can be found in the LICENSE file.
+
+  This module implements the Command class
+
+"""
+
 import sys
 
+
+
 class Command(object):
+
+    """
+        Creates a command object that provides the execution of the cli commands
+        & command chaining
+
+        Attributes:
+            use (str): the name of the command (mandatory)
+            short (str): short description of the command
+            long (str): long description of the command
+            run (str): the method to run when the command is executed
+            commands ([]Command): list of sub-commands
+            parent (Command): the parent command
+            arg_pos (int): number of parent commands to determine the argument
+                           postion in terms of sys.argv
+
+
+        Example:
+
+            root_command = pikli.Command(
+                    use = "hello",
+                    short = "Hello is the  first ever cli app built using pikli",
+            )
+
+
+    """
 
     def __init__(self , use , short = None , long = None , run = None):
         self.use = use
@@ -9,49 +49,98 @@ class Command(object):
         self.run = run
         self.commands = []
         self.parent = None
-        self.arguments = 0
+        self.arg_pos = 0
 
     def execute(self):
-        self.arguments = self.__parent_count()
+
+        """ Performs the execution of the command
+
+            Example:
+                root_command = pikli.Command(
+                        use = "hello",
+                        short = "Hello is the  first ever cli app built using pikli",
+                )
+                root_command.execute()
+
+        """
+
+        self.arg_pos = self.__parent_count()
 
         self.__check_run()
 
         self.__check_short()
 
-        self.__check_commands()
+        self.__check_available_commands()
 
         self.__check_sub_commands()
 
 
     def add_command(self , cmnd):
+
+        """ Adds a sub command under the current command
+
+            Args:
+                cmnd (Command): the command to be added as the sub-command
+
+            Example:
+                root_command = pikli.Command(
+                        use = "hello",
+                        short = "Hello is the  first ever cli app built using pikli",
+                )
+
+                migration_command = pikli.Command(
+                        use = "migration",
+                        short = "Runs Database migrations"
+                )
+                root_command.add_command(migration_command)
+
+        """
+
         cmnd.parent = self
         self.commands.append(cmnd)
 
     def __parent_count(self):
+
+        """ Calculates the number of parents in the whole chain & returns the
+            count
+        """
+
         count = 0
-        parent = self
+        parent = self #a single node is still a parent without a child
         while parent:
             count += 1
             parent = parent.parent
         return count
 
     def __check_run(self):
+
+        """ Checks for a run method & acts accordingly """
+
         if self.run:
             try:
                 self.run(self , sys.argv)
             except Exception:
-                pass
+                pass  # TODO: Add explicit exceptions
     def __check_short(self):
-        if self.short and not self.run and len(sys.argv) == self.arguments:
+
+        """ Checks for a short description & prints it"""
+
+        if self.short and not self.run and len(sys.argv) == self.arg_pos: #if short description provided & nothing to run & no args
             print(self.short + "\n\n\n")
-    def __check_commands(self):
-        if self.commands and len(sys.argv) == self.arguments:
+    def __check_available_commands(self):
+
+        """ Checks for avaiable commands to display them"""
+
+        if self.commands and len(sys.argv) == self.arg_pos:
             print("Available Commands:")
             for command in self.commands:
                 print("{}            {}".format(command.use,command.short))
     def __check_sub_commands(self):
-        if len(sys.argv) > self.arguments and self.commands:
+
+        """ Checks for any sub commands from the cli for execution """
+
+        if len(sys.argv) > self.arg_pos and self.commands: #if sub-command sys.argv position is ok & sub-commands actually exists
             for command in self.commands:
-                if command.use == sys.argv[self.arguments]:
+                if command.use == sys.argv[self.arg_pos]: #if the sub-command.use is the command provided
                     command.execute()
                     break
